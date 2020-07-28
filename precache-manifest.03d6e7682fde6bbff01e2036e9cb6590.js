20c9738d7ba5d8e3020e18d782e789c7f628cd44
Amr Elnaggar <amr.eln2gar@gmail.com>
2020-07-28 17:44:10 +0200

    Updates

---
+++ precache-manifest.03d6e7682fde6bbff01e2036e9cb6590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cab8a75026d4308ab1394b46bb254c",
+    "url": "/nusm/index.html"
+  },
+  {
+    "revision": "cf91d152233ef8776f69",
+    "url": "/nusm/static/css/main.559c8791.chunk.css"
+  },
+  {
+    "revision": "f573c26ac6352941793a",
+    "url": "/nusm/static/js/2.cc3980d5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nusm/static/js/2.cc3980d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf91d152233ef8776f69",
+    "url": "/nusm/static/js/main.79984874.chunk.js"
+  },
+  {
+    "revision": "f1e6c87e86c870cdbf6c",
+    "url": "/nusm/static/js/runtime-main.30855a34.js"
+  }
+]);
